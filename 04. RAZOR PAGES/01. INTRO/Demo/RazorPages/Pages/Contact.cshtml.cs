35310dbc6cba4cd874f5c9e5f4d2f4611fac7276
Zdravko Zdravkov <zdr0@yahoo.com>
2020-07-02 17:48:37 +0200

    Contact page with Razor Pages

---
+++ 04. RAZOR PAGES/01. INTRO/Demo/RazorPages/Pages/Contact.cshtml.cs
@@ -0,0 +1,54 @@
+﻿namespace RazorPages.Pages
+{
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    
+    /// <summary>
+    /// Razor Pages support Dependency injection
+    /// </summary>
+    public class ContactModel : PageModel
+    {
+        [BindProperty]
+        public ContactInput Input { get; set; }
+
+        public string Info { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            if (this.ModelState.IsValid)
+            {
+                this.Info = "Your message was successfully submitted";
+            }
+            else
+            {
+                this.Info = "All fields are required.";
+            }
+        }
+
+        public class ContactInput
+        {
+            [Required]
+            [MinLength(3)]
+            public string Name { get; set; }
+
+            [Required]
+            [DataType(DataType.EmailAddress)]
+            [MinLength(6)]
+            public string Email { get; set; }
+
+            [Required]
+            [MinLength(2)]
+            public string Title { get; set; }
+
+            [Required]
+            [MinLength(10)]
+            public string Content { get; set; }
+        }
+    }
+}
