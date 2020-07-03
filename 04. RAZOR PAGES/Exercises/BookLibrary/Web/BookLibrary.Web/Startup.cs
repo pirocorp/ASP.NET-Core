ad10d74985f4986baed847532156b072d4c8e1b0
Zdravko Zdravkov <zdr0@yahoo.com>
2020-07-03 16:02:31 +0200

    Add bundle and minifier support

--- 04. RAZOR PAGES/Exercises/BookLibrary/Web/BookLibrary.Web/Startup.cs
+++ 04. RAZOR PAGES/Exercises/BookLibrary/Web/BookLibrary.Web/Startup.cs
@@ -18,7 +18,7 @@ namespace BookLibrary.Web
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
