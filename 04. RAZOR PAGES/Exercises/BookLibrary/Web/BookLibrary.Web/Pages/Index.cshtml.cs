9c716121ce8c27bbfc2c8587f3497d7a5120daf1
Zdravko Zdravkov <zdr0@yahoo.com>
2020-07-04 12:28:42 +0200

    01. Basics-and-Razor-Pages-Exercises

--- 04. RAZOR PAGES/Exercises/BookLibrary/Web/BookLibrary.Web/Pages/Index.cshtml.cs
+++ 04. RAZOR PAGES/Exercises/BookLibrary/Web/BookLibrary.Web/Pages/Index.cshtml.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-
-namespace BookLibrary.Web.Pages
+﻿namespace BookLibrary.Web.Pages
 {
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
--- 04. RAZOR PAGES/Exercises/BookLibrary/Web/BookLibrary.Web/Startup.cs
+++ 04. RAZOR PAGES/Exercises/BookLibrary/Web/BookLibrary.Web/Startup.cs
@@ -1,20 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
 namespace BookLibrary.Web
 {
-    using Data;
+    using System;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Http;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    using Data;
 
     public class Startup
     {
@@ -48,7 +43,10 @@ namespace BookLibrary.Web
             IWebHostEnvironment env,
             BookLibraryDbContext context)
         {
-            context.Database.Migrate();
+            if (env.IsDevelopment())
+            {
+                context.Database.Migrate();
+            }
 
             if (env.IsDevelopment())
             {
