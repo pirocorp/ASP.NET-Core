911078c33bdaecbab99c131ac1a0caa2bc3647e8
Zdravko Zdravkov <zdr0@yahoo.com>
2020-07-05 15:27:18 +0200

    Book Add form

--- 04. RAZOR PAGES/Exercises/BookLibrary/Web/BookLibrary.Web/Pages/Books/Add.cshtml.cs
+++ 04. RAZOR PAGES/Exercises/BookLibrary/Web/BookLibrary.Web/Pages/Books/Add.cshtml.cs
@@ -1,12 +1,28 @@
 ﻿namespace BookLibrary.Web.Pages.Books
 {
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
 
     public class AddModel : PageModel
     {
+        [BindProperty]
+        public InputModel Input { get; set; }
+
         public void OnGet()
         {
 
+        }
+
+
+        public class InputModel
+        {
+            public string Title { get; set; }
+
+            public string Description { get; set; }
+
+            public string CoverImage { get; set; }
+
+            public string Author { get; set; }
         }
     }
 }
