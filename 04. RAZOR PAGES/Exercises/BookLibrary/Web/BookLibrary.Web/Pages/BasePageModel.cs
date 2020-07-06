010ad87f69a0f1fb8723ccd5edb35d0ff9fed719
Zdravko Zdravkov <zdr0@yahoo.com>
2020-07-06 19:39:54 +0200

    Extracting common Page logic in base class

---
+++ 04. RAZOR PAGES/Exercises/BookLibrary/Web/BookLibrary.Web/Pages/BasePageModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BookLibrary.Web.Pages
+{
+    using Data;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public abstract class BasePageModel : PageModel
+    {
+        protected readonly BookLibraryDbContext _db;
+
+        protected BasePageModel(BookLibraryDbContext db)
+        {
+            this._db = db;
+        }
+    }
+}
