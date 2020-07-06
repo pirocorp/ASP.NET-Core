f26c8e862dde95807030dc214a7a54914b83389e
Zdravko Zdravkov <zdr0@yahoo.com>
2020-07-06 18:09:00 +0200

    Author details and search

---
+++ 04. RAZOR PAGES/Exercises/BookLibrary/Web/BookLibrary.Web/Pages/Authors/Details.cshtml.cs
@@ -0,0 +1,63 @@
+﻿namespace BookLibrary.Web.Pages.Authors
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Data;
+    using Data.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class DetailsModel : PageModel
+    {
+        private readonly BookLibraryDbContext _db;
+
+        public DetailsModel(BookLibraryDbContext db)
+        {
+            this._db = db;
+            this.Output = new OutputModel();
+        }
+
+        public OutputModel Output { get; set; }
+
+        public IActionResult OnGet(int id)
+        {
+            var author = this._db.Authors.Find(id);
+
+            if (author is null)
+            {
+                return this.NotFound();
+            }
+
+            this.Output.AuthorName = author.Name;
+
+            this.Output.Books = this._db
+                .Books
+                .Where(b => b.AuthorId == id)
+                .Select(b => new BookDisplayModel()
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Status = b.Status,
+                })
+                .ToList();
+
+            return this.Page();
+        }
+
+        public class OutputModel
+        {
+            public List<BookDisplayModel> Books { get; set; }
+
+            public string AuthorName { get; set; }
+        }
+
+        public class BookDisplayModel
+        {
+            public int Id { get; set; }
+
+            public string Title { get; set; }
+
+            public BookStatus Status { get; set; }
+        }
+    }
+}
---
+++ 04. RAZOR PAGES/Exercises/BookLibrary/Web/BookLibrary.Web/Pages/Search.cshtml.cs
@@ -0,0 +1,70 @@
+﻿namespace BookLibrary.Web.Pages
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Data;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class SearchModel : PageModel
+    {
+        private readonly BookLibraryDbContext _db;
+
+        public SearchModel(BookLibraryDbContext db)
+        {
+            this._db = db;
+
+            this.Output = new OutputModel();
+        }
+
+        public OutputModel Output { get; set; }
+
+        public void OnGet(string searchTerm)
+        {
+            var results = new List<ListingModel>();
+
+            var authors = this._db
+                .Authors
+                .Where(a => a.Name.ToLower().Contains(searchTerm.ToLower()))
+                .Select(a => new ListingModel()
+                {
+                    Id = a.Id,
+                    Name = a.Name,
+                    Source = "author",
+                })
+                .ToList();
+
+            var books = this._db
+                .Books
+                .Where(b => b.Title.ToLower().Contains(searchTerm.ToLower()))
+                .Select(b =>  new ListingModel()
+                {
+                    Id = b.Id,
+                    Name = b.Title,
+                    Source = "book",
+                })
+                .ToList();
+
+            results.AddRange(authors);
+            results.AddRange(books);
+
+            this.Output.Listings = results;
+            this.Output.SearchTerm = searchTerm;
+        }
+
+        public class OutputModel
+        {
+            public IEnumerable<ListingModel> Listings { get; set; }
+
+            public string SearchTerm { get; set; }
+        }
+
+        public class ListingModel
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string Source { get; set; }
+        }
+    }
+}
