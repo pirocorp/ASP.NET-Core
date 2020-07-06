5049d7cc5f3e6a0079466fc80a894d58935c99aa
Zdravko Zdravkov <zdr0@yahoo.com>
2020-07-06 17:07:01 +0200

    List all books

--- 04. RAZOR PAGES/Exercises/BookLibrary/Web/BookLibrary.Web/Pages/Index.cshtml.cs
+++ 04. RAZOR PAGES/Exercises/BookLibrary/Web/BookLibrary.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,54 @@
 ﻿namespace BookLibrary.Web.Pages
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using Data;
+    using Data.Models;
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
-    using Microsoft.Extensions.Logging;
 
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly BookLibraryDbContext _db;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(BookLibraryDbContext db)
         {
-            _logger = logger;
+            this._db = db;
         }
+
+        public List<BookDisplayModel> Books { get; set; }
 
         public void OnGet()
         {
+            this.Books = this._db.Books
+                .OrderBy(b => b.Title)
+                .Select(b => new BookDisplayModel()
+                {
+                    BookId = b.Id,
+                    Title = b.Title,
+                    AuthorName = b.Author.Name,
+                    AuthorId = b.AuthorId,
+                    Status = b.Status
+                })
+                .ToList();
+        }
 
+        public IActionResult OnPost()
+        {
+            return this.RedirectToPage("/Books/Add");
+        }
+
+        public class BookDisplayModel
+        {
+            public int BookId { get; set; }
+
+            public string Title { get; set; }
+
+            public int AuthorId { get; set; }
+
+            public string AuthorName { get; set; }
+
+            public BookStatus Status { get; set; }
         }
     }
 }
