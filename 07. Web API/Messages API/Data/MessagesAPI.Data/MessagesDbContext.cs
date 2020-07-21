e469875d35bf1a1cb9572e42d85246dfe210d87a
Zdravko Zdravkov <zdr0@yahoo.com>
2020-07-21 18:10:37 +0200

    Add InitialCreate

--- 07. Web API/Messages API/Data/MessagesAPI.Data/MessagesDbContext.cs
+++ 07. Web API/Messages API/Data/MessagesAPI.Data/MessagesDbContext.cs
@@ -1,11 +1,26 @@
 ﻿namespace MessagesAPI.Data
 {
     using Microsoft.EntityFrameworkCore;
+    using Models;
 
     public class MessagesDbContext : DbContext
     {
         public MessagesDbContext(DbContextOptions<MessagesDbContext> options)
             : base(options)
         { }
+
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // Needed for Identity models configuration
+            base.OnModelCreating(builder);
+
+            this.ConfigureRelations(builder);
+        }
+
+        // Applies configurations
+        private void ConfigureRelations(ModelBuilder builder)
+            => builder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
     }
 }
---
+++ 07. Web API/Messages API/Data/MessagesAPI.Data/Migrations/20200721160846_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿namespace MessagesAPI.Data.Migrations
+{
+    using System;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Content = table.Column<string>(nullable: true),
+                    User = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
