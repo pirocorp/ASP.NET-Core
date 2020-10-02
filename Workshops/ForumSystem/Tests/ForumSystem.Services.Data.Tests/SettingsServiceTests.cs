c7cece3771017b6329803d18681aa483a4aaa08c
Zdravko Zdravkov <zdr0@yahoo.com>
2020-10-02 11:40:39 +0200

    Update unit test

--- Workshops/ForumSystem/Tests/ForumSystem.Services.Data.Tests/SettingsServiceTests.cs
+++ Workshops/ForumSystem/Tests/ForumSystem.Services.Data.Tests/SettingsServiceTests.cs
@@ -18,15 +18,19 @@
         public void GetCountShouldReturnCorrectNumber()
         {
             var repository = new Mock<IDeletableEntityRepository<Setting>>();
-            repository.Setup(r => r.All()).Returns(new List<Setting>
-                                                        {
-                                                            new Setting(),
-                                                            new Setting(),
-                                                            new Setting(),
-                                                        }.AsQueryable());
+            repository
+                .Setup(r => r.AllAsNoTracking())
+                .Returns(new List<Setting>
+                {
+                    new Setting(),
+                    new Setting(),
+                    new Setting(),
+                }
+                .AsQueryable());
             var service = new SettingsService(repository.Object);
+
             Assert.Equal(3, service.GetCount());
-            repository.Verify(x => x.All(), Times.Once);
+            repository.Verify(x => x.AllAsNoTracking(), Times.Once);
         }
 
         [Fact]
@@ -34,10 +38,10 @@
         {
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
                 .UseInMemoryDatabase(databaseName: "SettingsTestDb").Options;
-            using var dbContext = new ApplicationDbContext(options);
-            dbContext.Settings.Add(new Setting());
-            dbContext.Settings.Add(new Setting());
-            dbContext.Settings.Add(new Setting());
+            await using var dbContext = new ApplicationDbContext(options);
+            await dbContext.Settings.AddAsync(new Setting());
+            await dbContext.Settings.AddAsync(new Setting());
+            await dbContext.Settings.AddAsync(new Setting());
             await dbContext.SaveChangesAsync();
 
             using var repository = new EfDeletableEntityRepository<Setting>(dbContext);
