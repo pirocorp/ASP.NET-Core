8d2db4cb804c4061374a9d06ec781603aa8694b8
Zdravko Zdravkov <zdr0@yahoo.com>
2020-10-08 15:04:51 +0200

    LSP

---
+++ 10. DESIGN PATTERNS/Solid/Demos/03. Liskov Substitution/P02. Virtuals-Before/Animal.cs
@@ -0,0 +1,18 @@
+﻿namespace P02._Virtuals_Before
+{
+    public class Animal
+    {
+        // ReSharper disable VirtualMemberCallInConstructor
+        public Animal(string name = null)
+        {
+            this.Name = name ?? this.SetDefaultName();
+        }
+
+        public string Name { get; private set; }
+
+        protected virtual string SetDefaultName()
+        {
+            return "Animal";
+        }
+    }
+}
---
+++ 10. DESIGN PATTERNS/Solid/Demos/03. Liskov Substitution/P02. Virtuals-Before/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P02._Virtuals_Before
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animal = new Animal();
+
+            Console.WriteLine(animal.Name);
+
+            var cat = new Cat();
+
+            Console.WriteLine(cat.Name);
+        }
+    }
+}
