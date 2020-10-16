0f66ac64022afa21b365f616f7e2972c6b747b5b
Zdravko Zdravkov <zdr0@yahoo.com>
2020-10-16 19:22:35 +0200

    Testing template method and add moment js client library

---
+++ Workshops/ForumSystem/Tests/ForumSystem.Services.Tests/TestViewModels/MyTestPostViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ForumSystem.Services.Tests.TestViewModels
+{
+    using ForumSystem.Data.Models;
+    using Mapping;
+
+    public class MyTestPostViewModel : IMapFrom<Post>
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+    }
+}
