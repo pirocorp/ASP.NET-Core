5831e99b21f2cfa61533a818352a5f9e33f8af05
Zdravko Zdravkov <zdr0@yahoo.com>
2020-10-23 10:58:39 +0200

    Alter Table PackageStatuses column Name is unique and not null

---
+++ Workshops/Panda/Panda.Data/Configurations/PackageStatusesConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace Panda.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using Panda.Models;
+
+    public class PackageStatusesConfiguration : IEntityTypeConfiguration<PackageStatus>
+    {
+        public void Configure(EntityTypeBuilder<PackageStatus> packageStatus)
+        {
+            packageStatus
+                .HasIndex(ps => ps.Name)
+                .IsUnique();
+        }
+    }
+}
