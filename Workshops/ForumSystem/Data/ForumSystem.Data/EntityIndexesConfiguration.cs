af5c3dcf32a2c13f8c1ac8a49d3d64b079a9630e
Zdravko Zdravkov <zdr0@yahoo.com>
2020-10-26 18:01:19 +0100

    new line

--- Workshops/ForumSystem/Data/ForumSystem.Data/EntityIndexesConfiguration.cs
+++ Workshops/ForumSystem/Data/ForumSystem.Data/EntityIndexesConfiguration.cs
@@ -16,6 +16,7 @@
             var deletableEntityTypes = modelBuilder.Model
                 .GetEntityTypes()
                 .Where(et => et.ClrType != null && typeof(IDeletableEntity).IsAssignableFrom(et.ClrType));
+
             foreach (var deletableEntityType in deletableEntityTypes)
             {
                 modelBuilder.Entity(deletableEntityType.ClrType).HasIndex(nameof(IDeletableEntity.IsDeleted));
