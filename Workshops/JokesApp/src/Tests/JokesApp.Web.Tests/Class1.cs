107da763b6d7340ca59ea98fe8331cf7c2e8462f
Zdravko Zdravkov <zdr0@yahoo.com>
2020-11-12 12:51:00 +0100

    Testing examples

---
+++ Workshops/JokesApp/src/Tests/JokesApp.Web.Tests/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace JokesApp.Web.Tests
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc.Testing;
+    using Xunit;
+
+    public class Class1 : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> server;
+
+        public Class1(WebApplicationFactory<Startup> server)
+        {
+            this.server = server;
+        }
+
+        [Fact]
+        public async Task IndexPageShouldReturnStatus200()
+        {
+            var client = this.server.CreateClient();
+            var response = await client.GetAsync("/");
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.Contains("<title>", content);
+        }
+    }
+}
