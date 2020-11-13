483ceba9504e0c806256505f07c1ddec1c444a0b
Zdravko Zdravkov <zdr0@yahoo.com>
2020-11-13 12:43:54 +0100

    Initial project with identity and migrations setup

---
+++ Workshops/LearningSystem/LearningSystem.Web/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace LearningSystem.Web.Infrastructure.Extensions
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ServiceCollectionExtensions
+    {
+        public static IdentityBuilder AddLearningSystemIdentity<TUser>(this IServiceCollection services)
+            where TUser : IdentityUser 
+        {
+            return services.AddDefaultIdentity<TUser>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequiredUniqueChars = 0;
+                    options.Password.RequiredLength = 6;
+
+                    options.User.RequireUniqueEmail = true;
+
+                    options.SignIn.RequireConfirmedEmail = false;
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.SignIn.RequireConfirmedPhoneNumber = false;
+                });
+        }
+    }
+}
---
+++ Workshops/LearningSystem/LearningSystem.Web/Startup.cs
@@ -0,0 +1,75 @@
+namespace LearningSystem.Web
+{
+    using Data;
+    using Infrastructure.Extensions;
+
+    using LearningSystem.Data.Models;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<LearningSystemDbContext>(options =>
+                options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddLearningSystemIdentity<User>()
+                .AddEntityFrameworkStores<LearningSystemDbContext>();
+
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
+
+            services.AddRazorPages();
+
+            // Filters
+            services.AddDatabaseDeveloperPageExceptionFilter(); // This filter is used to produce developer exception page
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                // This two method are added in .net 5 instead of app.UseDatabaseErrorPage();
+                // In services DeveloperPageExceptionFilter must be registered
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
