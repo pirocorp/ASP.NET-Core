ec49d4455fac0492c7225f8ae452bb0ed887f9c5
Zdravko Zdravkov <zdr0@yahoo.com>
2020-11-17 17:29:10 +0100

    User profile visualization with signed in courses and grades

--- Workshops/LearningSystem/LearningSystem.Web/Models/Users/UserProfileUserModel.cs
+++ Workshops/LearningSystem/LearningSystem.Web/Models/Users/UserProfileUserModel.cs
@@ -14,7 +14,7 @@
             this.Courses = new List<UserProfileCourseModel>();
         }
 
-        public string Username { get; set; }
+        public string UserName { get; set; }
 
         public string Name { get; set; }
 
